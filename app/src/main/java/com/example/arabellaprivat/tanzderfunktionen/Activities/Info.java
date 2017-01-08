ce203dbb179d2f4e5457638b065f99d00769a815
Kathi <katharina.baumgartner@t-online.de>
2017-01-08 20:45:35 +0100

    Info als Pop-Up

--- app/src/main/java/com/example/arabellaprivat/tanzderfunktionen/Activities/Info.java
+++ app/src/main/java/com/example/arabellaprivat/tanzderfunktionen/Activities/Info.java
@@ -1,8 +1,11 @@
 package com.example.arabellaprivat.tanzderfunktionen.activities;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,7 +15,7 @@ import com.example.arabellaprivat.tanzderfunktionen.R;
 /**
  * hier werden Tipps zum Lösen der Aufgaben gegeben
  */
-public class Info extends AppCompatActivity {
+public class Info extends Activity {
 
 
     /** zeigt den Hilfetext an */
@@ -39,6 +42,17 @@ public class Info extends AppCompatActivity {
         // Intent, das diese Activity geöffnet hat holen
         Intent i = getIntent();
         Bundle b = i.getExtras();
+
+
+        // Größe beeinflussen
+        DisplayMetrics dm= new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width =dm.widthPixels;
+        int heigth= dm.heightPixels;
+
+        getWindow().setLayout((int)(width*0.8),(int)(heigth*0.5));
+
 
         // entsprechende Info zum jeweiligen Level wird aus der Klasse Spiel übergeben
         // TODO static Instanzen und Methoden non-static machen!!
